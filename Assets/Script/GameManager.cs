92753d189732ddc2bc911341a24bc2a5a1e8c406
Sakuralalala <32644480+Sakuralalala@users.noreply.github.com>
2018-06-30 15:36:14 +0800

    道具随机刷新

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -9,23 +9,41 @@ public class GameManager : MonoBehaviour {
 
     public GameObject[] revivePlace;
     public GameObject[] tanks;
+    public GameObject[] propPlace;
 
     [Range(0,5)][SerializeField][Header("坦克复活时间")]
     private float reviveTime;
+
+    [Range(0, 5)]
+    [SerializeField]
+    [Header("道具刷新时间")]
+    private float refalshTime;
+
+    [SerializeField ][Header("是否刷新")]
+    private bool isRefalsh = false;
 
     private void Awake()
     {
         instance = this;
         tanks = GameObject.FindGameObjectsWithTag("Tank");
+       
+    }
+    private void Start()
+    {
+        StartCoroutine(RefalshProp());
     }
 
     private void Update()
     {
-        foreach(GameObject tank in tanks)
+        RefalshProp();
+        foreach (GameObject tank in tanks)
         {
             if(!tank.GetComponent<Tank>().IsAlive)
             {
-                StartCoroutine(Revive(tank));
+                Vector3 revive = revivePlace[Random.Range(0, revivePlace.Length)].transform.position;
+                tank.transform.position = revive;
+                StartCoroutine(Revive(tank, revive));
+               
             }
         }
     }
@@ -35,16 +53,72 @@ public class GameManager : MonoBehaviour {
 
     }
 
-    IEnumerator Revive(GameObject tank)
+    IEnumerator Revive(GameObject tank, Vector3 revive)
     {
-        yield return reviveTime;
-        tank.transform.position = revivePlace[Random.Range(0, revivePlace.Length)].transform.position;
+        yield return new WaitForSeconds(reviveTime);    
         tank.SetActive(true);
         tank.GetComponent<Tank>().IsAlive = true;
         tank.GetComponent<Tank>().Downgrade();
         tank.GetComponent<Tank>().CurrentHP = tank.GetComponent<TankData>().HP;
-        yield return 0;
+        
     }
+
+    IEnumerator  RefalshProp()
+    {
+
+        int index = GameObject.Find("PropRefalsh").transform.childCount;
+        List<GameObject> propPosList = new List<GameObject>();
+        for(int i = 0; i < index; i++)
+        {
+            propPosList.Add(GameObject.Find("PropRefalsh").transform.GetChild(i).gameObject);
+        }
+        propPlace = propPosList.ToArray();
+
+        GameObject addHpProp = Resources.Load<GameObject>("AddHpProp");
+        GameObject addBigBulletProp = Resources.Load<GameObject>("AddBigBulletProp");
+        GameObject addInvincibleProp = Resources.Load<GameObject>("AddInvincibleProp");
+        GameObject addAttackSpeedProp = Resources.Load<GameObject>("AddAttackSpeedProp");
+        GameObject addSpeedProp = Resources.Load<GameObject>("AddSpeedProp");
+
+        if (GameObject.FindGameObjectsWithTag("Prop").Length == 0)
+        {
+            foreach (GameObject p in propPlace)
+            {
+                
+                int range = Random.Range(0, propPlace.Length);
+                switch (range)
+                {
+                    
+                    case 0: Instantiate(addHpProp, p.transform.position, p.transform.rotation); break;
+                    case 1: Instantiate(addBigBulletProp, p.transform.position, p.transform.rotation); break;
+                    case 2: Instantiate(addInvincibleProp, p.transform.position, p.transform.rotation); break;
+                    case 3: Instantiate(addAttackSpeedProp, p.transform.position, p.transform.rotation); break;
+                    case 4: Instantiate(addSpeedProp, p.transform.position, p.transform.rotation); break;
+                }
+
+            }
+        }
+        yield return new WaitForSeconds(refalshTime);
+        yield return DestoryProp();
+
+
+    }
+    
+    IEnumerator DestoryProp()
+    {
+        if (GameObject.FindGameObjectsWithTag("Prop").Length != 0)
+        {
+            GameObject[] gameObjects = GameObject.FindGameObjectsWithTag("Prop");
+            foreach (GameObject g in gameObjects)
+            {
+                Destroy(g);
+            }
+        }
+        yield return new WaitForSeconds(refalshTime);
+        yield return RefalshProp();
+    }
+
+
 
 
 }
